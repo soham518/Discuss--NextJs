9114118f078a8408f8c72f6ff79cbff757ad2b41
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-24 22:06:24 +0530

    add zod validation schema for createTopic action

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,7 +1,16 @@
 "use server"
+import {z} from "zod";
 
+const createTopicSchema = z.object({
+    name:z.string().min(3),
+    discription:z.string().min(10)
+})
 export const createTopic = async (formData: FormData) => {
   const name = formData.get('name');
   const discription = formData.get('discription');
-  console.log(name,discription);
+  const result = createTopicSchema.safeParse({
+    name:formData.get('name'),
+    discription:formData.get('discription')
+  });
+  
 }
